3589a4a5b03f3240ca77d805bb518759d176311a
lucasgranjense23 <23.lucasdoliveira@gmail.com>
2024-10-08 21:59:05 -0300

    Feat:Hero image

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,10 @@
 import { CoffeList } from "../../components/coffe/CoffeList";
 import { Container } from "./styles";
-
+import Hero from '../../assets/Intro.svg'
 export function Home() {
     return (
     <Container>
+        <img src={Hero} alt="Hero"  />
         <h1>Nossos cafés</h1>
         <CoffeList />
     </Container>
