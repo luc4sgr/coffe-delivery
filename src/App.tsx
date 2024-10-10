22bc55bff43ebb6a1b4bf189ce14862ad469d1a8
lucasgranjense23 <23.lucasdoliveira@gmail.com>
2024-10-09 22:37:21 -0300

    Feat:Aplicando provaider dos detalhes do pedido

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { CoffeesProvider } from "./context/CoffeesContext";
 import { Router } from "./routes";
 import { CartProvider } from "./context/CartContext";
+import { PurchaseDetailsProvider } from "./context/PurchaseDetailsContext";
 
 
 export function App() {
@@ -13,9 +14,11 @@ export function App() {
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
         <CartProvider>
+        <PurchaseDetailsProvider>
           <CoffeesProvider>
             <Router />
           </CoffeesProvider>
+        </PurchaseDetailsProvider>
         </CartProvider>
       </BrowserRouter>
       <GlobalStyle />
